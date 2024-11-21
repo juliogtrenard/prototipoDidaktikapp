ab7475dbd92fbd4dc79ab400e3a66985636c66ab
intibal29 <baloukintissar@gmail.com>
2024-11-21 09:54:12 +0100

    ADD actividad de ayuda

--- app/src/main/java/es/juliogtrenard/prototipoprincipal/MainActivity.kt
+++ app/src/main/java/es/juliogtrenard/prototipoprincipal/MainActivity.kt
@@ -1,11 +1,27 @@
 package es.juliogtrenard.prototipoprincipal
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import es.juliogtrenard.prototipoprincipal.actividades.AyudaActivity
+import es.juliogtrenard.prototipoprincipal.actividades.JugarActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cardViewJugar: CardView = findViewById(R.id.card_jugar)
+        cardViewJugar.setOnClickListener {
+            val intent = Intent(this, JugarActivity::class.java)
+            startActivity(intent)
+        }
+
+        val cardViewAyuda: CardView = findViewById(R.id.card_ayuda)
+        cardViewAyuda.setOnClickListener {
+            val intent = Intent(this, AyudaActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/es/juliogtrenard/prototipoprincipal/actividades/AyudaActivity.kt
+++ app/src/main/java/es/juliogtrenard/prototipoprincipal/actividades/AyudaActivity.kt
@@ -2,23 +2,22 @@ package es.juliogtrenard.prototipoprincipal.actividades
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.postDelayed
 import es.juliogtrenard.prototipoprincipal.MainActivity
 import es.juliogtrenard.prototipoprincipal.R
 
-class SplashActivity : AppCompatActivity() {
+class AyudaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_ayuda)
 
-        val handler = Handler(Looper.getMainLooper())
-        handler.postDelayed(1200) {
+        val botonVolver = findViewById<Button>(R.id.botonAyuda_volver)
+
+        botonVolver.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+
             startActivity(intent)
-            finish()
         }
     }
 }
